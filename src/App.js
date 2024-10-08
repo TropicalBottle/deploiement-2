0e5a88919bca401d633d889169e806b77ca90e87
Alexandre Blanquero <aleaxndre.blanquero00@gmail.com>
2024-10-08 14:36:33 +0200

    feat: clean app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import Counter from "./components/Counter";
-import {useState} from "react";
+import Form from "./components/Form/Form";
 
 function App() {
-  // const [count, setCount] = useState(0);
-  //
-  // const ClickOnMe = () => {
-  //   setCount(count+1);
-  // }
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Counter/>
-
-        {/*<button onClick={ClickOnMe}>click me</button>*/}
-        {/*<br/>*/}
-        {/*<span data-testid="count">{count}</span>*/}
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Form/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
