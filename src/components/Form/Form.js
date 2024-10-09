60d33bbea992741ae756ed73e03815f1ac8ac072
Alexandre Blanquero <aleaxndre.blanquero00@gmail.com>
2024-10-09 16:48:02 +0200

    fix: register button typo

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -108,7 +108,7 @@ export default function Form() {
 
                     <button disabled={ !isButtonAvailable }
                             className={ "text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 disabled:cursor-not-allowed disabled:bg-gray-300 disabled:text-gray-500" }
-                            type="submit">Submit
+                            type="submit">Register
                     </button>
                 </form>
             </div>
