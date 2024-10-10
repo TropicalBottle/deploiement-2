5a093ffff7325667295789690bbc56b149c6e3f6
Alexandre Blanquero <aleaxndre.blanquero00@gmail.com>
2024-10-10 08:32:31 +0200

    fix: change toast text

--- src/components/Toast/SuccessToast.js
+++ src/components/Toast/SuccessToast.js
@@ -12,7 +12,7 @@ export default function SuccessToast({ setSuccessful, progress }) {
                 </svg>
                 <span className="sr-only">Check icon</span>
             </div>
-            <div className="ms-3 text-sm font-normal">Item moved successfully.</div>
+            <div className="ms-3 text-sm font-normal">You have been registered.</div>
             <button type="button"
                     className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8"
                     onClick={ () => setSuccessful( false ) }
